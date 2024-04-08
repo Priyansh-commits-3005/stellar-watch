247a6bbe60cffc1b9b6102d9f147555f0387ab88
Aum Barge <tomybarge@gmail.com>
2024-04-08 14:02:36 +0530

    adding the frontend files

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Landing from "./landing"
+import Navbar from "./Navbar"
+function App() {
+
+  return (
+      <>
+        <Navbar />
+        <Landing />
+      </>
+  )
+}
+
+export default App
---
+++ my-project/src/landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='h-screen w-full bg-black flex items-center justify-center '>
+      <h1 className='animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black'>
+        Explore the Universe with Stellar Watch
+      </h1>
+    </div>
+  )
+}
+
+export default Landing
